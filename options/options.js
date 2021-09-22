572ffd9c65dff4e528c67afc10b47bb45e98b95f
Crowdbotics <team@crowdbotics.com>
2021-09-22 19:38:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "summer_fog_30756Identifier",
+  url: "https://summer_fog_30756Identifier.botics.co",
+  api: "https://summer_fog_30756Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
